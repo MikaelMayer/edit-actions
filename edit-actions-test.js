MikaelMayer/edit-actions

commit f844e083a60a06b9c104c691bf32513eeec23d44

Latest updates

edit-actions.js
@@ -536,7 +536,7 @@ var editActions = {};
      let ap = lens.apply;
      lens.apply = function(input, r, rCtx) {
        lens.cachedInput = input;
-       let output = ap(input, r, rCtx);
+       let output = ap.bind(lens)(input, r, rCtx);
        lens.cachedOutput = output;
        return output;
      }
@@ -812,6 +812,9 @@ var editActions = {};
     if(arguments.length == 2) {
       second = first;
       first = Reuse();
+    }
+    if(!isEditAction(second)) { // Otherwise it cause problems when composing.
+      second = New(second);
     }
     return Concat(count, first, second, undefined, true, false);
   }
@@ -1323,8 +1326,9 @@ var editActions = {};
         } else {
           printDebug("is not extend")
           printDebug("recovered so far:", recovered);
+          let t = treeOpsOf(prog);
+          t.update(prog, k, applyMutateRecover(child, prog, Extend(recovered), ctx, AddContext(k, prog, resultCtx)));
           recovered[k] = Down(k, New(prog[k]));
-          prog[k] = applyMutateRecover(child, prog, Extend(recovered), ctx, AddContext(k, prog, resultCtx));
         }
       });
       return Extend(recovered);
@@ -3493,6 +3497,21 @@ var editActions = {};
          E2IsUp) {
         break merge_cases;   
       }
+      // No more New, Concat, Down or Up
+      // Only Reuse, Replace, and RemoveExcept now, and Custom
+      
+      let E1IsCustom = E1.ctor == Type.Custom;
+      let E2IsCustom = E2.ctor == Type.Custom;
+      // We only merge it once.
+      if(E1IsCustom && typeof E1.lens.merge == "function") {
+        result.push(E1.lens.merge(E1, E2));
+      } else if(E2IsCustom && typeof E2.lens.merge == "function") {
+        result.push(E2.lens.merge(E2, E1));
+      }
+      if(E1IsCustom ||
+         E2IsCustom) {
+        break merge_cases;   
+      }
       
       // No more New, Concat, Down or Up
       // Only Reuse, Replace, and RemoveExcept now.
@@ -4118,6 +4137,9 @@ var editActions = {};
         finalNexts.push(...nexts);
       }
       return [rawIfPossible(New(o, U.model), UWasRaw), finalNexts, ECtx, outCountU];
+    }
+    if(U.ctor == Type.Custom) {
+      return [U, [], ECtx, outCountU];
     }
     if(U.ctor == Type.Concat) {
       // Even replaces, we treat them like Concat when we try to resolve partitionEdit. At this point, we are already creating something new from old pieces.
@@ -4777,7 +4799,7 @@ var editActions = {};
     return $beforeEdit + $result;
   }
   editActions.serializeEdit = serializeEdit;
- function serializeWithoutStack(edit) {
+  function serializeWithoutStack(edit) {
     $i = 0;
     $stack = [];
     $result = "(function() { let x = StartArray();";
@@ -4794,10 +4816,10 @@ var editActions = {};
           if(edit.keyOrOffset.newLength === undefined) {
             $result += " x.Remove("+edit.keyOrOffset.count+");";
           } else {
-            $result += " x.RemoveExcept("+keyOrOffsetToString(edit.ek)+");";
+            $result += " x.RemoveExcept("+keyOrOffsetToString(edit.keyOrOffset)+");";
           }
         } else {
-          $result += " x.Down("+keyOrOffsetToString(edit.ek)+");";
+          $result += " x.Down("+keyOrOffsetToString(edit.keyOrOffset)+");";
         }
         
         edit = edit.subAction;
@@ -6254,7 +6276,9 @@ var editActions = {};
     RecordLike: {
       init() { return {}; },
       access(x, k) { return x[k]; },
-      update(x, k, v) { x[k] = v; },
+      update(x, k, v) { 
+        x[k] = v;
+      },
       forEach(x, callback) {
         for(let k in x) {
           callback(x[k], numIfPossible(k), x);
@@ -6270,6 +6294,7 @@ var editActions = {};
       }
     }
   }
+  editActions.treeOps = treeOps;
   const identity = Reuse();
   editActions.identity = identity;
   function treeOpsOf(x) {
@@ -6573,6 +6598,7 @@ var editActions = {};
     if(isNumeric(num)) return Number(num);
     return num;
   }
+  editActions.numIfPossible = numIfPossible;
   
   function stringOf(self) {
     if(Array.isArray(self)) {
@@ -7073,6 +7099,7 @@ var editActions = {};
   transform.isExtend = isExtend;
   transform.isDown = isDown;
   transform.isUp = isUp;
+  transform.isReplace = isReplace;
   transform.valueIfConst = valueIfConst;
   transform.isOffset = isOffset;
   transform.downDownOffset = downDownOffset;

edit-actions-test.js
@@ -10,6 +10,36 @@ var testsPassed = 0; linesFailed = [], incompleteLines = [];
 var bs = "\\\\";
 var failAtFirst = true;
 
+function CustomIncrement(d) {
+  let LensName = "CustomIncrement";
+  return Custom({value: Reuse(), increment: d}, {
+      name: LensName,
+      apply(x) {
+        return x.value + x.increment;
+      },
+      merge(custom1, custom2) {
+        if(custom1.ctor == Type.Custom && custom1.lens.name == LensName
+           && custom2.ctor == Type.Custom && custom2.lens.name == LensName) {
+          return CustomIncrement(custom1.subAction.increment + custom2.subAction.increment);
+        }
+        return Choose(custom1, custom2);
+      }
+    }
+  );
+}
+
+var customMerged = merge(CustomIncrement(1), CustomIncrement(2));
+shouldBeEqual(apply(customMerged, 0), 3);
+customMerged = merge(CustomIncrement(4), Reuse());
+shouldBeEqual(apply(customMerged, 0), 4);
+customMerged = merge(Reuse(), CustomIncrement(4));
+shouldBeEqual(apply(customMerged, 0), 4);
+customMerged = merge(New(3), CustomIncrement(4));
+shouldBeEqual(apply(customMerged, 0), 3);
+customMerged = merge(CustomIncrement(4), New(3));
+shouldBeEqual(apply(customMerged, 0), 3);
+
+
 edit = Reuse({x: Up("x", Down("y", New([Choose({a: New(2), b: Choose(New(3), New(4))}, New(1))])))});
 shouldBeEqual(
   firstRewrite(edit),

commit c832716926c2733f69b0629d071918686092cbd7

Fix: Append preceded by a remove is now a replace Previously, `Up(X, Remove(X, ...))` was not simplified because the case never arose in practice. I found a case when it does, so this simplification needs to take place. Also, I fixed a failing test due to a bug introduced in f844e083a60a06b9c104c691bf32513eeec23d44

edit-actions.js
@@ -289,8 +289,8 @@ var editActions = {};
             } else {
               return Up(newOffset, subAction.subAction);
             }
-          } 
-        } else if(subAction.ctor == Type.Down && !subAction.isRemove) {
+          }
+        } else if(subAction.ctor == Type.Down) {
           let isk = isOffset(subAction.keyOrOffset);
           if(!ik && !isk) {
             if(keyOrOffset == subAction.keyOrOffset) {
@@ -301,7 +301,11 @@ var editActions = {};
             if(isOffsetIdentity(newOffset)) return subAction.subAction;
             let newDownOffset = upToDownOffset(newOffset);
             if(newDownOffset !== undefined) {
-              return Down(newDownOffset, subAction.subAction);
+              if(subAction.isRemove) {
+                return Remove(newDownOffset, subAction.subAction);
+              } else {
+                return Down(newDownOffset, subAction.subAction);
+              }
             }
             return Up(newOffset, subAction.subAction);
           }
@@ -1328,8 +1332,8 @@ var editActions = {};
           printDebug("is not extend")
           printDebug("recovered so far:", recovered);
           let t = treeOpsOf(prog);
-          t.update(prog, k, applyMutateRecover(child, prog, Extend(recovered), ctx, AddContext(k, prog, resultCtx)));
           recovered[k] = Down(k, New(prog[k]));
+          t.update(prog, k, applyMutateRecover(child, prog, Extend(recovered), ctx, AddContext(k, prog, resultCtx)));
         }
       });
       return Extend(recovered);

edit-actions-test.js
@@ -3,12 +3,18 @@ var {New,Concat,Up,Down,Custom,UseResult,Choose,Clone,
      Offset, Interval, Extend,
      apply, andThen, merge, backPropagate,
      isIdentity, stringOf, diff, first, debug, reverse} = editActions;
-var {List,Reuse,Replace,Keep,Prepend, Append,Drop,DropAll,DropAfter,Remove,RemoveExcept,RemoveAll,KeepOnly,Type,__AddContext,__ContextElem,isOffset,uneval, splitAt, downAt, offsetAt, Sequence, ReuseOffset, Insert, InsertAll, ExtendModel, ReuseAsIs, transform, mergeInto, StartArray, firstRewrite} = editActions;
+var {List,Reuse,Replace,Keep,Prepend, Append,Drop,DropAll,DropAfter,Remove,RemoveExcept,RemoveAll,KeepOnly,Type,__AddContext,__ContextElem,isOffset,uneval, splitAt, downAt, offsetAt, Sequence, ReuseOffset, Insert, InsertAll, ExtendModel, ReuseAsIs, transform, mergeInto, StartArray, firstRewrite, strDiff} = editActions;
+var Create = New;
 
 var tests = 0, testToStopAt = undefined;
 var testsPassed = 0; linesFailed = [], incompleteLines = [];
 var bs = "\\\\";
 var failAtFirst = true;
+
+var c = Remove(1);
+var a = Replace(1, 2, Append(1, New("X")));
+var c2 = andThen(c, a);
+shouldBeEqual(c2, Replace(1, 1, Create("X")));
 
 function CustomIncrement(d) {
   let LensName = "CustomIncrement";
@@ -184,6 +190,14 @@ shouldBeEqual(
   v, {a: "a", d: {a: "prev", d: "x", c: "c"}}
 );
 
+var v = {a: { c: 1 }, b: {d: 2}}
+editActions.applyMutate(
+  Reuse({a: Reuse({c: Up("c", "a", Down("b", "d"))}), b: Reuse({d: Up("d", "b", Down("a"))})}), v);
+/*shouldBeEqual(
+  v, {a: { c: 2 }, b: {d: { c: 1 }}}
+);*/
+
+
 shouldBeEqual(
 andThen(
 Reuse({stack: New(1)}),
@@ -220,13 +234,6 @@ shouldBeEqual(
     state: New({}),
     local_vars: New({
       name: Up("local_vars", Down.pure("state", 5))})}));
-
-var v = {a: { c: 1 }, b: {d: 2}}
-editActions.applyMutate(
-  Reuse({a: Reuse({c: Up("c", "a", Down("b", "d"))}), b: Reuse({d: Up("d", "b", Down("a"))})}), v);
-shouldBeEqual(
-  v, {a: { c: 2 }, b: {d: { c: 1 }}}
-);
 
 /*
 shouldBeEqual(
@@ -523,7 +530,7 @@ shouldBeEqual(stringOf(New([1, 2, 3], partialArray)), "New([1, 2, 3], [WRAP, NEW
 shouldBeEqual(apply(Reuse({a: Reuse({b: New(1)}), d: 2}), {a: {b: 2}}), {a: {b: 1}, d: 2});
 
 shouldBeEqual(
-  stringOf(Append(3, "abc")), "Append(3, \"abc\")" 
+  stringOf(Append(3, "abc")), "Append(3, New(\"abc\"))" 
 );
 shouldBeEqual(
   stringOf(Append(3, Prepend(1, "d"), New("abc"))), "Append(3, \n  Prepend(1, \"d\"),\n  New(\"abc\"))" 